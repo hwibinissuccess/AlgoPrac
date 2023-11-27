72d545eafaee54683f39125b8012f4c106eed4a2
Choi Hwi bin <chlgnlqls3@nate.com>
2023-11-27 22:27:03 +0900

    "S3_11047_동전개수의최솟값구하기" solve

---
+++ BJ/그리디/S3_11047_동전개수의최솟값구하기.java
@@ -0,0 +1,27 @@
+package BJ.그리디;
+
+import java.util.Scanner;
+
+public class S3_11047_동전개수의최솟값구하기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0; i<N; i++){
+            A[i] = sc.nextInt();
+        }
+
+        int cnt = 0;
+
+        for(int i = N-1; i>=0; i--){
+            if(A[i]<=K){
+                cnt +=(K/A[i]);
+                K = K%A[i];
+            }
+        }
+        System.out.println(cnt);
+    }
+
+}
