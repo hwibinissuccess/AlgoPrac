7244f6dd3b8795aa2e7ef49fb4c633e75ae299da
Choi Hwi bin <chlgnlqls3@nate.com>
2023-12-14 16:23:44 +0900

    "S5_1934_최소공배수구하기" solve

---
+++ BJ/유클리드호제법/S5_1934_최소공배수구하기.java
@@ -0,0 +1,24 @@
+package BJ.유클리드호제법;
+
+import java.util.Scanner;
+
+public class S5_1934_최소공배수구하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int result = a*b/gcd(a,b);
+            System.out.println(result);
+        }
+    }
+
+    public static int gcd(int a, int b){
+        if(b==0)
+            return a;
+        else
+            return gcd(b,a%b);
+    }
+
+}
