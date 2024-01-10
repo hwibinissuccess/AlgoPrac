1bfaaee87adba7caca63d35bfd93826b9e256430
Choi Hwi bin <chlgnlqls3@nate.com>
2024-01-10 19:41:32 +0900

    "G3_1516_게임개발하기","G3_2252_줄세우기" solve

---
+++ BJ/위상정렬/G3_1516_게임개발하기ㅇㅇㅇ.java
@@ -0,0 +1,56 @@
+package BJ.위상정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class G3_1516_게임개발하기ㅇㅇㅇ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
+        for(int i=0; i<=N; i++){
+            A.add(new ArrayList<>());
+        }
+        int[] indegree = new int[N+1]; // 진입차수 배열
+        int[] selfBuild = new int[N+1]; // 자기 자신을 짓는데 걸리는 시간
+        for(int i=1; i<=N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            selfBuild[i] = Integer.parseInt(st.nextToken()); // 건물 짓는데 걸리는 시간
+            while(true){ // 인접 리스트 초기화
+                int preTemp = Integer.parseInt(st.nextToken());
+                if(preTemp == -1)
+                    break;
+                A.get(preTemp).add(i);
+                indegree[i]++; // 진입차수배열 초기화
+            }
+        }
+        //위상정렬
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1; i<=N; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        int[] result = new int[N+1];
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            for(int next: A.get(now)){
+                indegree[next]--;
+                // 시간 업데이트
+                result[next] = Math.max(result[next], result[now]+selfBuild[now]);
+                if(indegree[next] == 0){
+                    queue.offer(next);
+                }
+            }
+        }
+        for(int i=1; i<=N; i++){
+            System.out.println(result[i] + selfBuild[i]);
+        }
+    }
+
+}
