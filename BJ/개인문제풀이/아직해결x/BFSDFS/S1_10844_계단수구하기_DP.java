d78c60daca1ab153e509228d10b48cbfb7bfab17
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-08 00:55:37 +0900

    "S1_10844_계단수구하기_DP" 풀이

---
+++ BJ/개인문제풀이/아직해결x/BFSDFS/S1_10844_계단수구하기_DP.java
@@ -0,0 +1,44 @@
+package BJ.개인문제풀이.아직해결x.BFSDFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S1_10844_계단수구하기_DP {
+    static Long[][] dp;
+    static int N;
+    final static long MOD = 1000000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        dp = new Long[N+1][10];
+
+        for(int i=0; i<10; i++){
+            dp[1][i] = 1L;
+        }
+        long result=  0;
+
+        for(int i=1; i<=9; i++){
+            result += recure(N,i);
+        }
+        System.out.println(result % MOD);
+    }
+
+    static long recure(int d, int v){
+        if(d == 1){
+            return dp[d][v];
+        }
+        if(v==0){
+            dp[d][v] = recure(d-1,1);
+        } else if (v==9) {
+            dp[d][v] = recure(d-1,8);
+        }
+        else{
+            dp[d][v] = recure(d-1,v-1)+recure(d-1,v+1);
+        }
+        return dp[d][v]%MOD;
+    }
+
+}
