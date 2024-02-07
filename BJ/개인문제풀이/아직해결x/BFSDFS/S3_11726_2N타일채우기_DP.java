ebb71e79b407bd8aa65186f4746a5a098e8e1900
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-07 19:03:55 +0900

    "S3_11726_2N타일채우기_DP" 풀이

---
+++ BJ/개인문제풀이/아직해결x/BFSDFS/S3_11726_2N타일채우기_DP.java
@@ -0,0 +1,19 @@
+package BJ.개인문제풀이.아직해결x.BFSDFS;
+
+import java.util.Scanner;
+
+public class S3_11726_2N타일채우기_DP {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<n; i++){
+            dp[i] = (dp[i-1]+dp[i-2])%10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
