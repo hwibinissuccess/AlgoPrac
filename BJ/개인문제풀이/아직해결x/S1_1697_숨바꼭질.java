340b8dcd7670d1cf5fa1fe9443aa7e15d5f11f88
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-10 21:08:11 +0900

    "S1_1697_숨바꼭질" 풀이

--- BJ/개인문제풀이/아직해결x/S1_1697_숨바꼭질.java
+++ BJ/개인문제풀이/아직해결x/S1_1697_숨바꼭질.java
@@ -5,37 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class S1_1697_숨바꼭질_BFS {
-    static int N,K;
-    static int visit[] = new int[100001];
+public class S1_1697_숨바꼭질 {
+    static int N, K;
+    static int[] visit = new int[100001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        String[] inputs = input.split(" ");
-
-        N = Integer.valueOf(inputs[0]);
-        K = Integer.valueOf(inputs[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
         int result = BFS(N);
         System.out.println(result);
+
     }
 
-    private static int BFS(int node){
-        Queue<Integer> q = new LinkedList<Integer>();
+    public static int BFS(int node){
+        Queue<Integer> q = new LinkedList<>();
 
         q.add(node);
         int index = node;
         int n=0;
         visit[index] = 1;
         while(!q.isEmpty()){
-            n = q.remove();
-
-            if(n == K){
+            n = q.poll();
+            if(n==K){
                 return visit[n]-1;
             }
-            if(n-1>=0 && visit[n-1]==0){
+            if(n-1>=0 && visit[n-1] == 0){
                 visit[n-1] = visit[n]+1;
                 q.add(n-1);
             }
@@ -43,9 +42,9 @@ public class S1_1697_숨바꼭질_BFS {
                 visit[n+1] = visit[n]+1;
                 q.add(n+1);
             }
-            if(n*2<=100000 && visit[n*2] == 0){
+            if(n*2<=100000 && visit[2*n] == 0){
                 visit[2*n] = visit[n]+1;
-                q.add(n*2);
+                q.add(2*n);
             }
         }
         return -1;
