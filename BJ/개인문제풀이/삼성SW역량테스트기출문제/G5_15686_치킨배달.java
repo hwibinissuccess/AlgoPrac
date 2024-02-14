77cee55f3a2ed6c6a2082744369c1ca722af05ff
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-14 16:43:46 +0900

    "G5_15686_치킨배달" 풀이

---
+++ BJ/개인문제풀이/삼성SW역량테스트기출문제/G5_15686_치킨배달.java
@@ -0,0 +1,93 @@
+package BJ.개인문제풀이.삼성SW역량테스트기출문제;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class Point{
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class G5_15686_치킨배달 {
+    static int N,M;
+    static int[][] map;
+    static ArrayList<Point> person;
+    static ArrayList<Point> chicken;
+    static int ans;
+    static boolean[] open;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+        person = new ArrayList<>();
+        chicken = new ArrayList<>();
+
+        //. 미리 집과 치킨집에 해당하는 좌표를 ArrayList에 넣어두기
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(st.nextToken());
+            for(int j=0; j<N; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+
+                if(map[i][j] == 1){
+                    person.add(new Point(i,j));
+                } else if(map[i][j] == 2){
+                    chicken.add(new Point(i,j));
+                }
+            }
+        }
+
+        ans = Integer.MAX_VALUE;
+        open = new boolean[chicken.size()];
+
+        DFS(0,0);
+        bw.write(ans+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    public static void DFS(int start, int cnt){
+        if(cnt == M){
+            int res=0;
+
+            for(int i=0; i<person.size(); i++){
+                int temp = Integer.MAX_VALUE;
+
+                // 어떤 집과 치킨 집 중 open한 치킨집의 모든 거리를 비교
+                // 그 중 최소 거리 구하기
+                for(int j=0; j<chicken.size(); j++){
+                    if(open[j]){
+                        int distance = Math.abs(person.get(i).x - chicken.get(j).x)+Math.abs(person.get(i).y - chicken.get(j).y);
+
+                        temp = Math.min(temp, distance);
+                    }
+                }
+                res += temp;
+            }
+            ans = Math.min(ans, res);
+            return;
+        }
+
+        for(int i=start; i<chicken.size(); i++){
+            open[i] = true;
+            DFS(i+1, cnt+1);
+            open[i] = false;
+        }
+
+    }
+
+}
