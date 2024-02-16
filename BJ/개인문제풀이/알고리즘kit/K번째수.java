6959268c479ed630fc4a2834268e537a74d7b059
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-16 21:44:11 +0900

    "알고리즘Kit_K번째수" 풀이

---
+++ BJ/개인문제풀이/알고리즘kit/K번째수.java
@@ -0,0 +1,21 @@
+package BJ.개인문제풀이.알고리즘kit;
+
+import java.util.Arrays;
+
+public class K번째수 {
+
+    public int[] solution(int[] array, int[][] commands) {
+
+        int[] ans = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            ans[i] = temp[commands[i][2]-1];
+        }
+
+        return ans;
+
+    }
+
+}
