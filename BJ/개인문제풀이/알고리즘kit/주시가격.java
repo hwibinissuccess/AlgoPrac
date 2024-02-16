3e2159a295eeb47d1d1f90d8fd19c65dc22cc5b2
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-16 17:02:02 +0900

    "알고리즘Kit_주시가격" 풀이

---
+++ BJ/개인문제풀이/알고리즘kit/주시가격.java
@@ -0,0 +1,26 @@
+package BJ.개인문제풀이.알고리즘kit;
+
+import java.util.Stack;
+
+public class 주시가격 {
+
+    public int[] solution(int[] prices) {
+
+        int[] ans = new int[prices.length];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0; i<prices.length; i++){
+            while(!st.isEmpty() && prices[i]<prices[st.peek()]){
+                ans[st.peek()] = i-st.peek();
+                st.pop();
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty()){
+            ans[st.peek()] = prices.length - st.peek()-1;
+            st.pop;
+        }
+        return ans;
+    }
+
+}
