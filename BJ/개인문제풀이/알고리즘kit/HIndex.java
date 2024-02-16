f953d26f5ae254b5efcf2a021f6e91a614559960
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-16 22:18:49 +0900

    "알고리즘Kit_HIndex" 풀이

---
+++ BJ/개인문제풀이/알고리즘kit/HIndex.java
@@ -0,0 +1,24 @@
+package BJ.개인문제풀이.알고리즘kit;
+
+import java.util.Arrays;
+
+public class HIndex {
+
+    public int solution(int[] citations) {
+
+        int ans = 0;
+        Arrays.sort(citations);
+
+        for(int i=0; i<citations.length; i++){
+            int h = citations.length-i;
+
+            if(citations[i] >= h){
+                ans = h;
+                break;
+            }
+        }
+        return ans;
+
+    }
+
+}
