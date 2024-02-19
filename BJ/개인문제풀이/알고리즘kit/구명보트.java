8ef349c6d16c11e3a9841ee30b0baa9170149192
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-19 22:41:20 +0900

    "알고리즘Kit_구명보트" 풀이

---
+++ BJ/개인문제풀이/알고리즘kit/구명보트.java
@@ -0,0 +1,25 @@
+package BJ.개인문제풀이.알고리즘kit;
+
+import java.util.Arrays;
+
+public class 구명보트 {
+
+    public int solution(int[] people, int limit) {
+
+        int ans = 0;
+        Arrays.sort(people);
+        int index=0;
+
+        for(int i=people.length-1; i>=index; i--){
+            if(people[i] + people[index] <= limit){
+                index++;
+                ans++;
+            } else{
+                ans++;
+            }
+        }
+        return ans;
+
+    }
+
+}
