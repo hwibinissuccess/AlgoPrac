b3076effc70127ebff19acec648adc5902449f07
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-19 19:49:17 +0900

    "알고리즘Kit_전력망을둘로나누기" 풀이

---
+++ BJ/개인문제풀이/알고리즘kit/전력망을둘로나누기.java
@@ -0,0 +1,60 @@
+package BJ.개인문제풀이.알고리즘kit;
+
+import java.util.ArrayList;
+
+public class 전력망을둘로나누기 {
+
+
+    static ArrayList<Integer>[] graph;
+    static int min;
+
+    public int solution(int n, int[][] wires) {
+
+        graph = new ArrayList[n+1];
+        min = Integer.MAX_VALUE;
+
+        for(int i=1; i<=n; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<wires.length; i++){
+            int v1 = wires[i][0];
+            int v2 = wires[i][1];
+            graph[v1].add(v2);
+            graph[v2].add(v1);
+        }
+
+        for(int i=0; i<wires.length; i++){
+            int v1 = wires[i][0];
+            int v2 = wires[i][1];
+
+            boolean[] visit = new boolean[n+1];
+
+            graph[v1].remove(Integer.valueOf(v2));
+            graph[v2].remove(Integer.valueOf(v1));
+
+            int cnt = DFS(1, visit);
+
+            int diff = Math.abs(cnt-(n-cnt));
+            min = Math.min(min, diff);
+
+            graph[v1].add(v2);
+            graph[v2].add(v1);
+        }
+        return min;
+
+    }
+
+    static int DFS(int v, boolean[] visit){
+        visit[v] = true;
+        int cnt = 1;
+
+        for(int next : graph[v]){
+            if(!visit[next]){
+                cnt += DFS(next, visit);
+            }
+        }
+        return cnt;
+    }
+
+}
