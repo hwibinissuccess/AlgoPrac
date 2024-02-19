eefc5acb965f16099b72c4933126ddc144d5b41b
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-19 22:54:55 +0900

    "알고리즘Kit_섬연결하기" 풀이

---
+++ BJ/개인문제풀이/알고리즘kit/섬연결하기.java
@@ -0,0 +1,50 @@
+package BJ.개인문제풀이.알고리즘kit;
+
+import java.util.Arrays;
+
+public class 섬연결하기 {
+
+
+    class Solution {
+
+        static int[] parent;
+
+        public int solution(int n, int[][] costs) {
+
+            int ans = 0;
+            parent = new int[n];
+
+            for(int i=0; i<n; i++){
+                parent[i] = i;
+            }
+
+            Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
+
+            for(int i=0; i<costs.length; i++){
+                if(find(costs[i][0]) != find(costs[i][1])){
+                    union(costs[i][0], costs[i][1]);
+
+                    ans += costs[i][2];
+                }
+            }
+
+            return ans;
+
+        }
+
+        public static int find(int a){
+            if(parent[a] == a) return a;
+            else return parent[a] = find(parent[a]);
+        }
+
+        public static void union(int a, int b){
+            a = find(a);
+            b = find(b);
+            if(a != b){
+                parent[b] = a;
+            }
+        }
+
+    }
+
+}
