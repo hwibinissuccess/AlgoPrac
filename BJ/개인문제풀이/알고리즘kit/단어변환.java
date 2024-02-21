7cad7df2be2375ddc1bf6b6ea186507e7c50d72f
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-21 18:54:45 +0900

    "알고리즘Kit_단어변환" 풀이

---
+++ BJ/개인문제풀이/알고리즘kit/단어변환.java
@@ -0,0 +1,46 @@
+package BJ.개인문제풀이.알고리즘kit;
+
+public class 단어변환 {
+
+
+    static boolean[] visit;
+    static int ans = 0;
+
+    public int solution(String begin, String target, String[] words) {
+
+        visit = new boolean[words.length];
+
+        DFS(begin, target, words, 0);
+        return ans;
+    }
+
+    public static void DFS(String begin, String target, String[] words, int cnt){
+        if(begin.equals(target)){
+            ans = cnt;
+            return;
+        }
+
+        for(int i=0; i<words.length; i++){
+            if(visit[i]){
+                continue;
+            }
+
+            int k = 0; // 같은 스펠링 세기
+            for(int j=0; j<begin.length(); j++){
+                if(begin.charAt(j) == words[i].charAt(j)){
+                    k++;
+                }
+            }
+
+            if(k == begin.length() -1){
+                visit[i] = true;
+                DFS(words[i], target, words, cnt+1);
+                visit[i] = false;
+            }
+
+        }
+
+    }
+
+
+}
