dcdd8ce4bbca48351951423fe0b96085e7306e2b
Choi Hwi bin <chlgnlqls3@nate.com>
2024-02-29 22:39:21 +0900

    "알고리즘Kit_가장큰수" 해결

---
+++ BJ/개인문제풀이/알고리즘kit/해결/가장큰수.java
@@ -0,0 +1,32 @@
+package BJ.개인문제풀이.알고리즘kit.해결;
+
+import java.util.Arrays;
+
+public class 가장큰수 {
+
+    public String solution(int[] numbers) {
+
+        String[] temp = new String[numbers.length];
+
+        for(int i=0; i<numbers.length; i++){
+            temp[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(temp, (o1, o2) -> (o2+o1).compareTo(o1+o2));
+
+        if(temp[0].equals("0")){
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<temp.length; i++){
+            sb.append(temp[i]);
+        }
+
+
+        return sb.toString();
+
+    }
+
+}
