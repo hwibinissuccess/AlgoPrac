8a48438307e943c445b883c78563e7ff6171e3e7
Choi Hwi bin <chlgnlqls3@nate.com>
2024-03-10 20:23:50 +0900

    "게임맵최단거리" 해결

--- BJ/개인문제풀이/알고리즘kit/디스크컨트롤러_N.java
+++ BJ/개인문제풀이/알고리즘kit/디스크컨트롤러_N.java
@@ -1,4 +1,4 @@
 package BJ.개인문제풀이.알고리즘kit;
 
-public class 디스크컨트롤러 {
+public class 디스크컨트롤러_N {
 }
--- BJ/개인문제풀이/알고리즘kit/해결/게임맵최단거리.java
+++ BJ/개인문제풀이/알고리즘kit/해결/게임맵최단거리.java
@@ -1,30 +1,32 @@
-package BJ.개인문제풀이.알고리즘kit;
+package BJ.개인문제풀이.알고리즘kit.해결;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class 게임맵최단거리 {
 
-    int[] dx = {1,0,-1,0};
-    int[] dy = {0,1,0,-1};
+    static int[][] visit;
+    static int[] dx = {1,0,-1,0};
+    static int[] dy = {0,1,0,-1};
 
     public int solution(int[][] maps) {
 
-        int ans = 0;
+        visit = new int[maps.length][maps[0].length];
 
-        int[][] visit = new int[maps.length][maps[0].length];
-
-        BFS(maps, visit);
-        ans = visit[maps.length-1][maps[0].length-1];
+        BFS(maps,visit);
+        int ans = visit[maps.length-1][maps[0].length-1];
 
         if(ans == 0){
             ans = -1;
         }
+
         return ans;
+
 
     }
 
-    public void BFS(int[][] maps, int[][] visit){
+    public static void BFS(int[][] maps, int[][] visit){
+
         int x = 0;
         int y = 0;
         visit[x][y] = 1;
@@ -32,7 +34,8 @@ public class 게임맵최단거리 {
         q.add(new int[]{x,y});
 
         while(!q.isEmpty()){
-            int[] cur = q.remove();
+
+            int[] cur = q.poll();
             int cx = cur[0];
             int cy = cur[1];
 
@@ -40,16 +43,18 @@ public class 게임맵최단거리 {
                 int nx = cx+dx[i];
                 int ny = cy+dy[i];
 
-                if(nx < 0 || nx > maps.length-1 || ny < 0 || ny > maps[0].length-1)
-                    continue;
+                if(nx < 0 || ny < 0 || nx > maps.length-1 || ny > maps[0].length-1) continue;
 
                 if(visit[nx][ny] == 0 && maps[nx][ny] == 1){
                     visit[nx][ny] = visit[cx][cy]+1;
                     q.add(new int[]{nx,ny});
                 }
+
             }
+
         }
 
     }
+
 
 }
