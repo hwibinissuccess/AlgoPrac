1f2643c0c7fd952c237aa849eb3d140e95a5f4df
Choi Hwi bin <chlgnlqls3@nate.com>
2024-03-10 20:45:49 +0900

    "다리를지나는트럭" 해결

--- BJ/개인문제풀이/알고리즘kit/해결/다리를지나는트럭.java
+++ BJ/개인문제풀이/알고리즘kit/해결/다리를지나는트럭.java
@@ -1,4 +1,4 @@
-package BJ.개인문제풀이.알고리즘kit;
+package BJ.개인문제풀이.알고리즘kit.해결;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -7,34 +7,39 @@ public class 다리를지나는트럭 {
 
     public int solution(int bridge_length, int weight, int[] truck_weights) {
 
+        int time = 0;
+        int sum = 0;
         Queue<Integer> q = new LinkedList<>();
-        int sum=0;
-        int time=0;
 
         for(int i=0; i<truck_weights.length; i++){
+
             int truck = truck_weights[i];
 
             while(true){
+
                 if(q.isEmpty()){
                     q.add(truck);
-                    sum += truck;
                     time++;
+                    sum += truck;
                     break;
-                } else if(q.size() == bridge_length){
+                } else if(bridge_length == q.size()){
                     sum -= q.poll();
                 } else{
-                    if(sum+truck <= weight){
-                        q.add(truck);
-                        sum += truck;
-                        time++;
-                        break;
-                    } else{
+                    if(sum+truck > weight){
                         q.add(0);
                         time++;
+                    } else{
+                        q.add(truck);
+                        time++;
+                        sum += truck;
+                        break;
                     }
                 }
+
             }
+
         }
+
         return time+bridge_length;
 
     }
