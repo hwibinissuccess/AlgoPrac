5f511d2714a5beca241ba5b2fac9d1c270271b34
Choi Hwi bin <chlgnlqls3@nate.com>
2024-03-30 23:25:00 +0900

    "야근지수_1" 풀이

---
+++ BJ/개인문제풀이/PGM/야근지수_1.java
@@ -0,0 +1,34 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class 야근지수_1 {
+
+    public long solution(int n, int[] works) {
+
+        long ans = 0;
+
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0; i<works.length; i++){
+            q.add(works[i]);
+        }
+
+        for(int i=0; i<n; i++){
+            int max = (int)q.poll();
+            if(max <= 0) break;
+            q.add(max-1);
+        }
+
+
+        while(!q.isEmpty()){
+            ans += Math.pow(q.poll(),2);
+        }
+        return ans;
+
+
+
+    }
+
+}
