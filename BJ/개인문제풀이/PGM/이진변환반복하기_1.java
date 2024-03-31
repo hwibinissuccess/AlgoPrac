ac9ea28f67aa9a6cb012917ad708785572ed9171
Choi Hwi bin <chlgnlqls3@nate.com>
2024-04-01 00:18:09 +0900

    "이진변환반복하기" 풀이

---
+++ BJ/개인문제풀이/PGM/이진변환반복하기_1.java
@@ -0,0 +1,26 @@
+package BJ.개인문제풀이.PGM;
+
+public class 이진변환반복하기_1 {
+
+    public int[] solution(String s) {
+
+        int[] ans = new int[2];
+
+        while(s.length() > 1){
+
+            int cntOne = 0;
+            for(int i=0; i<s.length(); i++){
+                if(s.charAt(i) == '0') ans[1]++;
+                else cntOne++;
+            }
+
+            s = Integer.toBinaryString(cntOne);
+            ans[0]++;
+
+        }
+
+        return ans;
+
+    }
+
+}
