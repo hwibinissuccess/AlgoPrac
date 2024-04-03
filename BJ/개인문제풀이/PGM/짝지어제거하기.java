f9b6e19afb6d48832bbfa60eabcb08571ad4b298
Choi Hwi bin <chlgnlqls3@nate.com>
2024-04-03 18:22:31 +0900

    "짝지어제거하기" 해결

---
+++ BJ/개인문제풀이/PGM/짝지어제거하기.java
@@ -0,0 +1,33 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.Stack;
+
+public class 짝지어제거하기 {
+
+    public int solution(String s){
+
+        int ans = -1;
+        char[] carr = s.toCharArray();
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0 ;i<carr.length; i++){
+            char c = carr[i];
+
+            if(st.isEmpty()) st.push(c);
+            else{
+
+                if(st.peek() == c){
+                    st.pop();
+                } else{
+                    st.push(c);
+                }
+
+            }
+
+        }
+
+        return st.isEmpty() ? 1 : 0;
+
+    }
+
+}
