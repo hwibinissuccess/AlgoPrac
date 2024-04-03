2ba34b7b5e047c71c0d0dcd585ea0bad1c33b0fd
Choi Hwi bin <chlgnlqls3@nate.com>
2024-04-03 18:53:26 +0900

    "스티커모으기2" 해결

---
+++ BJ/개인문제풀이/PGM/해결/스티커모으기2.java
@@ -0,0 +1,37 @@
+package BJ.개인문제풀이.PGM.해결;
+
+public class 스티커모으기2 {
+
+    public long solution(int sticker[]) {
+
+        long ans = 0;
+
+        if(sticker.length == 1) return sticker[0];
+
+        long[] dp1 = new long[sticker.length];
+        long[] dp2 = new long[sticker.length];
+
+        dp1[0] = sticker[0];
+        dp1[1] = dp1[0];
+
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
+
+
+
+        for(int i=2; i<sticker.length; i++){
+            dp2[i] = Math.max(sticker[i]+dp2[i-2], dp2[i-1]);
+
+
+            if(i == sticker.length-1) break;
+
+            dp1[i] = Math.max(sticker[i]+dp1[i-2], dp1[i-1]);
+
+
+        }
+
+        return Math.max(dp1[sticker.length-2], dp2[sticker.length-1]);
+
+    }
+
+}
