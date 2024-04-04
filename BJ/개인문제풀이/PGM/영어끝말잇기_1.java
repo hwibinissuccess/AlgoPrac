6680dfe10e78794913b7aabcaa54781e5601a8ed
Choi Hwi bin <chlgnlqls3@nate.com>
2024-04-04 16:34:39 +0900

    "영어끝말잇기" 풀이

---
+++ BJ/개인문제풀이/PGM/영어끝말잇기_1.java
@@ -0,0 +1,32 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 영어끝말잇기_1 {
+
+    public int[] solution(int n, String[] words) {
+
+        int[] ans = new int[2];
+        List<String> list = new ArrayList<String>();
+        boolean flag = true;
+
+        for(int i=0; i<words.length; i++){
+            if(i>0 && words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0) ||
+                    list.contains(words[i])){
+                ans[0] = (i%n)+1;
+                ans[1] = (i/n)+1;
+                flag = false;
+                break;
+            }
+
+            list.add(words[i]);
+
+        }
+
+        if(flag) return new int[]{0,0};
+        return ans;
+
+    }
+
+}
--- BJ/개인문제풀이/PGM/짝지어제거하기_1_SSS.java
+++ BJ/개인문제풀이/PGM/짝지어제거하기_1_SSS.java
@@ -2,7 +2,7 @@ package BJ.개인문제풀이.PGM;
 
 import java.util.Stack;
 
-public class 짝지어제거하기_1 {
+public class 짝지어제거하기_1_SSS {
 
     public int solution(String s){
 
