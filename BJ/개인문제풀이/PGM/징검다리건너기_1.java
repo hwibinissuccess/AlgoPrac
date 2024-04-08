94ffae932f01161cc5feb4c5c2339d8ca17f0431
Choi Hwi bin <chlgnlqls3@nate.com>
2024-04-08 18:58:04 +0900

    "징검다리건너기" 풀이

---
+++ BJ/개인문제풀이/PGM/징검다리건너기_1.java
@@ -0,0 +1,44 @@
+package BJ.개인문제풀이.PGM;
+
+public class 징검다리건너기_1 {
+
+    public int solution(int[] stones, int k) {
+
+        int min = 0;
+        int max = Integer.MAX_VALUE;
+        int ans = 0;
+
+        while(min <= max){
+
+            int mid = (min+max)/2;
+
+            if(check(mid,k,stones)){
+                min = mid+1;
+                ans = mid;
+            } else{
+                max =mid-1;
+            }
+        }
+
+        return ans;
+
+    }
+
+    public static boolean check(int mid, int k, int[] stones){
+
+        int cnt = 0;
+
+        for(int i=0; i<stones.length; i++){
+
+            if(stones[i] < mid){
+                cnt++;
+                if(cnt >= k) return false;
+            } else cnt = 0;
+        }
+
+        return true;
+
+    }
+
+
+}
