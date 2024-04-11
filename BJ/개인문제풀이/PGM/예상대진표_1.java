b1d5fb66adecccf587c0bf8b75c6de9d5f82ba91
Choi Hwi bin <chlgnlqls3@nate.com>
2024-04-11 22:18:53 +0900

    "예상 대진표" 풀이

---
+++ BJ/개인문제풀이/PGM/예상대진표_1.java
@@ -0,0 +1,20 @@
+package BJ.개인문제풀이.PGM;
+
+public class 예상대진표_1 {
+
+    public int solution(int n, int a, int b){
+
+        int ans = 0;
+
+        while(true){
+            a = a/2+a%2;
+            b = b/2+b%2;
+            ans++;
+            if(a==b) break;
+        }
+
+        return ans;
+
+    }
+
+}
