40f273602acad9f1b9d37778f2232d17efab221e
Choi Hwi bin <chlgnlqls3@nate.com>
2024-04-15 17:47:53 +0900

    "가장긴팰린드롬" 풀이

---
+++ BJ/개인문제풀이/PGM/가장긴팰린드롬_1.java
@@ -0,0 +1,40 @@
+package BJ.개인문제풀이.PGM;
+
+public class 가장긴팰린드롬_1 {
+
+    public int solution(String s){
+
+        int ans = 1;
+
+        if(s.length() <= 1){
+            return 1;
+        }
+
+        for(int i=1; i<s.length(); i++){
+
+            if(s.charAt(i) == s.charAt(i-1)){
+                ans = Math.max(ans, Pal(s,i-1,i));
+            }
+
+            if(i>1 && s.charAt(i) == s.charAt(i-2)){
+                ans = Math.max(ans,Pal(s,i-2,i));
+            }
+
+        }
+
+        return ans;
+
+    }
+
+    public static int Pal(String s, int idx1, int idx2){
+
+        while(idx1>=0 && idx2<s.length() && s.charAt(idx1) == s.charAt(idx2)){
+            --idx1;
+            ++idx2;
+        }
+
+        return idx2-idx1-1;
+
+    }
+
+}
