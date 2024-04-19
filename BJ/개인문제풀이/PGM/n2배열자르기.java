373bb16d464340bbd7a22e3a174f440a5077ef58
Choi Hwi bin <chlgnlqls3@nate.com>
2024-04-19 17:38:14 +0900

    "n2배열자르기" 풀이

---
+++ BJ/개인문제풀이/PGM/n2배열자르기.java
@@ -0,0 +1,22 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class n2배열자르기 {
+
+    public List<Long> solution(int n, long left, long right) {
+
+        List<Long> list = new ArrayList<>();
+
+        for(long i=left; i<right+1; i++){
+
+            list.add(Math.max(i/n,i%n)+1);
+
+        }
+
+        return list;
+
+    }
+
+}
