72679f743f2eda388d6ff650549e5449e3a1ea96
Choi Hwi bin <chlgnlqls3@nate.com>
2024-04-29 23:11:18 +0900

    "거스름돈" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/거스름돈.java
@@ -0,0 +1,25 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.Arrays;
+
+public class 거스름돈 {
+
+    public int solution(int n, int[] money) {
+
+        int ans = 0;
+        Arrays.sort(money);
+
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for(int i=0; i<money.length; i++){
+            for(int j=money[i]; j<=n; j++){
+                dp[j] += dp[j-money[i]]%1000000007;
+            }
+        }
+
+        ans = dp[n];
+        return ans;
+
+    }
+
+}
