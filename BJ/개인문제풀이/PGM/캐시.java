0d883e00ac60a0358faa0374f28b9e826c368a82
Choi Hwi bin <chlgnlqls3@nate.com>
2024-04-29 22:50:24 +0900

    "캐시" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/캐시.java
@@ -0,0 +1,39 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.ArrayList;
+
+public class 캐시 {
+
+    public int solution(int cacheSize, String[] cities) {
+
+        int ans = 0;
+        ArrayList<String> cache = new ArrayList<>();
+
+        if(cacheSize == 0){
+            return cities.length*5;
+        }
+
+        for(int i=0; i<cities.length; i++){
+            String city = cities[i].toUpperCase();
+
+            if(cache.contains(city)){
+                cache.remove(city);
+                cache.add(city);
+                ans+=1;
+            } else{
+
+                if(cache.size() == cacheSize){
+                    cache.remove(0);
+                }
+
+                cache.add(city);
+                ans+=5;
+
+            }
+        }
+
+        return ans;
+
+    }
+
+}
