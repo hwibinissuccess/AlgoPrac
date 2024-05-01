a6aadc85f95a95071ea9e258131175bc98377348
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-01 23:06:35 +0900

    "행렬의곱셈" 해결

---
+++ BJ/개인문제풀이/PGM/해결/행렬의곱셈.java
@@ -0,0 +1,21 @@
+package BJ.개인문제풀이.PGM.해결;
+
+public class 행렬의곱셈 {
+
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+
+        int[][] ans = new int[arr1.length][arr2[0].length];
+
+        for(int i=0; i<arr1.length; i++){
+            for(int j=0; j<arr2[0].length; j++){
+                for(int k=0; k<arr1[0].length; k++){
+                    ans[i][j] += arr1[i][k]*arr2[k][j];
+                }
+            }
+        }
+
+        return ans;
+
+    }
+
+}
