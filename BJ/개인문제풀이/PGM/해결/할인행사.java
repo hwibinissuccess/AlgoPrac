e89ca2e03eddc5608809423d48b29c17c6b448c1
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-03 00:04:05 +0900

    "할인행사" 해결

--- BJ/개인문제풀이/PGM/해결/할인행사.java
+++ BJ/개인문제풀이/PGM/해결/할인행사.java
@@ -1,29 +1,30 @@
-package BJ.개인문제풀이.PGM;
+package BJ.개인문제풀이.PGM.해결;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class 할인행사_5 {
+public class 할인행사 {
 
     public int solution(String[] want, int[] number, String[] discount) {
 
         int ans = 0;
         Map<String, Integer> map = new HashMap<>();
-        int day = 10;
 
         for(int i=0; i<want.length; i++){
-            map.put(want[i],number[i]);
+            map.put(want[i], number[i]);
         }
+
+        int day = 10;
 
         for(int i=0; i<discount.length-day+1; i++){
 
-            Map<String,Integer> map2 = new HashMap<>();
+            Map<String, Integer> map2 = new HashMap<>();
 
-            for(int j=0; j<day; j++){
-                map2.put(discount[i+j], map2.getOrDefault(discount[i+j],0)+1);
+            for(int j=i; j<i+day; j++){
+                map2.put(discount[j], map2.getOrDefault(discount[j],0)+1);
             }
 
-            Boolean flag = true;
+            boolean flag = true;
 
             for(String key : map.keySet()){
                 if(map.get(key) != map2.get(key)){
@@ -32,7 +33,7 @@ public class 할인행사_5 {
                 }
             }
 
-            ans += flag ? 1 : 0;
+            if(flag) ans += 1;
 
         }
 
