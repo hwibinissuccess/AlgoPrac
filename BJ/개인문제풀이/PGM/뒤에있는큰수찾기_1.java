5bc01ba6e3443af3d6b515d0fc6704fb3f036045
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-03 00:37:12 +0900

    "뒤에있는큰수찾기" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/뒤에있는큰수찾기_1.java
@@ -0,0 +1,25 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class 뒤에있는큰수찾기_1 {
+
+    public int[] solution(int[] numbers) {
+
+        Stack<Integer> st = new Stack<>();
+
+        int[] ans = new int[numbers.length];
+        Arrays.fill(ans, -1);
+
+        for(int i=0; i<numbers.length; i++){
+            while(!st.isEmpty() && numbers[i] > numbers[st.peek()]){
+                ans[st.pop()] = numbers[i];
+            }
+            st.push(i);
+        }
+        return ans;
+
+    }
+
+}
