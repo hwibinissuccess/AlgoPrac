a54a5c060a72b59087141b7299bb8211e5360419
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-08 21:37:25 +0900

    "땅따먹기" 문제 해결

--- BJ/개인문제풀이/PGM/해결/땅따먹기.java
+++ BJ/개인문제풀이/PGM/해결/땅따먹기.java
@@ -1,18 +1,16 @@
-package BJ.개인문제풀이.PGM;
+package BJ.개인문제풀이.PGM.해결;
 
-public class 땅따먹기_2 {
+public class 땅따먹기 {
 
     int solution(int[][] land) {
 
         int ans = 0;
 
         for(int i=1; i<land.length; i++){
-
-            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2],land[i-1][3]));
             land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
             land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
             land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][1]));
-
         }
 
         for(int i=0; i<4; i++){
