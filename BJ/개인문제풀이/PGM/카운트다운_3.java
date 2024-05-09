afdb8ed41f37233bc158cdc2fde0f92682970371
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-09 17:46:06 +0900

    미해결 문제 풀이

--- BJ/개인문제풀이/PGM/카운트다운_3.java
+++ BJ/개인문제풀이/PGM/카운트다운_3.java
@@ -1,6 +1,6 @@
 package BJ.개인문제풀이.PGM;
 
-public class 카운트다운_2 {
+public class 카운트다운_3 {
     int[][] dp;
 
     public int[] solution(int target) {
--- BJ/개인문제풀이/PGM/합승택시요금_6.java
+++ BJ/개인문제풀이/PGM/합승택시요금_6.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class 합승택시요금_5 {
+public class 합승택시요금_6 {
 
     int N;
     int E;
