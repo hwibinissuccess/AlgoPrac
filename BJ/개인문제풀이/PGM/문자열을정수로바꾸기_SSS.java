7f19b757cb095a95fb079bfa8a90483a5297768e
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-12 19:24:55 +0900

    "문자열을정수로바꾸기" 문제 해결

---
+++ BJ/개인문제풀이/PGM/문자열을정수로바꾸기_SSS.java
@@ -0,0 +1,10 @@
+package BJ.개인문제풀이.PGM;
+
+public class 문자열을정수로바꾸기_SSS {
+    public int solution(String s) {
+
+        return Integer.parseInt(s);
+
+    }
+
+}
---
+++ BJ/개인문제풀이/PGM/해결/문자열을정수로바꾸기.java
@@ -0,0 +1,11 @@
+package BJ.개인문제풀이.PGM.해결;
+
+public class 문자열을정수로바꾸기 {
+
+    public int solution(String s) {
+
+        return Integer.parseInt(s);
+
+    }
+
+}
