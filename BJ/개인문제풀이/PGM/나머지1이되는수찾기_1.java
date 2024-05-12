8b01918056fe90d6d026649e4467619450e2b43a
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-12 20:50:15 +0900

    "나머지1이되는수찾기" 문제 해결

---
+++ BJ/개인문제풀이/PGM/나머지1이되는수찾기_1.java
@@ -0,0 +1,26 @@
+package BJ.개인문제풀이.PGM;
+
+public class 나머지1이되는수찾기_1 {
+
+    public int solution(int n) {
+
+        int ans = 0;
+
+        int x = 1;
+
+        while(true){
+
+            if(n%x == 1){
+                ans = x;
+                break;
+            }
+
+            x++;
+
+        }
+
+        return ans;
+
+    }
+
+}
