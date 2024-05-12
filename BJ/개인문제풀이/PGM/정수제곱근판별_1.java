2bcd38566b566ab3e1cd953e5adf42118efd0c0c
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-12 20:26:29 +0900

    "정수제곱근판별" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/정수제곱근판별_1.java
@@ -0,0 +1,22 @@
+package BJ.개인문제풀이.PGM;
+
+public class 정수제곱근판별_1 {
+
+    public long solution(long n) {
+
+        long ans = 0;
+
+        double x = Math.sqrt(n);
+
+        if(x%1 == 0){
+            ans = (long)Math.pow(x+1, 2);
+        } else return -1;
+
+        return ans;
+
+
+
+
+    }
+
+}
