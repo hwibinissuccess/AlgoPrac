22973661668ddb084575ece55b53bd5809104129
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-12 19:27:46 +0900

    "약수의합" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/약수의합_1.java
@@ -0,0 +1,19 @@
+package BJ.개인문제풀이.PGM;
+
+public class 약수의합_1 {
+
+    public int solution(int n) {
+
+        int ans = 0;
+
+        for(int i=1; i<=n; i++){
+
+            if(n%i == 0) ans += i;
+
+        }
+
+        return ans;
+
+    }
+
+}
