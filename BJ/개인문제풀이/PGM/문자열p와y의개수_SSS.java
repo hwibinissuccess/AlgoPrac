c5107272d446c2fa1abd3020f632da5534ec7d0a
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-12 19:22:52 +0900

    "문자열p와y의개수" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/문자열p와y의개수_SSS.java
@@ -0,0 +1,33 @@
+package BJ.개인문제풀이.PGM;
+
+public class 문자열p와y의개수_SSS {
+
+    boolean solution(String s) {
+
+        boolean ans = true;
+        char[] str = new char[s.length()];
+        s = s.toLowerCase();
+
+        for(int i=0; i<s.length(); i++){
+            str[i] = s.charAt(i);
+        }
+
+        int numP = 0;
+        int numY = 0;
+
+        for(int i=0; i<str.length; i++){
+
+            char ss= str[i];
+
+            if(ss== 'p') numP++;
+            if(ss== 'y') numY++;
+
+        }
+
+        if(numP == numY) return true;
+        else if(numP == 0 && numY == 0) return true;
+        else return false;
+
+    }
+
+}
