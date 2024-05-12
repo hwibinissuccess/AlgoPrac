0786425a672c776bfed3e2cd50e70755ded8dffd
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-12 20:15:52 +0900

    "정수내림차순으로배치하기" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/정수내림차순으로배치하기_1.java
@@ -0,0 +1,36 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.Arrays;
+
+public class 정수내림차순으로배치하기_1 {
+
+    public long solution(long n) {
+
+        String str = String.valueOf(n);
+
+        long ans = 0;
+
+        String[] temp = new String[str.length()];
+
+        for(int i=0; i<str.length(); i++){
+
+            temp[i] = String.valueOf(str.charAt(i));
+
+        }
+
+        Arrays.sort(temp, (o1, o2) -> Integer.parseInt(o2)-Integer.parseInt(o1));
+
+
+        String ans2 = "";
+
+        for(int i=0; i<str.length(); i++){
+
+            ans2 += temp[i];
+
+        }
+
+        return Long.parseLong(ans2);
+
+    }
+
+}
