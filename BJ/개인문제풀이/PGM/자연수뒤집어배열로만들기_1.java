ac6757ec03e95ffcba86609ca2473017d6ceda70
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-12 20:01:45 +0900

    "자연수뒤집어배열로만들기" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/자연수뒤집어배열로만들기_1.java
@@ -0,0 +1,24 @@
+package BJ.개인문제풀이.PGM;
+
+public class 자연수뒤집어배열로만들기_1 {
+
+    public int[] solution(long n) {
+
+        String str = String.valueOf(n);
+
+        int[] ans = new int[str.length()];
+        int idx = 0;
+
+        while(n>0){
+
+            ans[idx] = (int)(n%10);
+            n /= 10;
+            idx++;
+
+        }
+
+        return ans;
+
+    }
+
+}
