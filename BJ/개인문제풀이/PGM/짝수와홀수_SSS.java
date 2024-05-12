3be88736339ca0bfb38199de958a2581f2dc2208
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-12 20:28:01 +0900

    "짝수와홀수" 문제 해결

---
+++ BJ/개인문제풀이/PGM/짝수와홀수_SSS.java
@@ -0,0 +1,15 @@
+package BJ.개인문제풀이.PGM;
+
+public class 짝수와홀수_SSS {
+
+    public String solution(int num) {
+
+        if(num%2 == 0){
+            return "Even";
+        } else{
+            return "Odd";
+        }
+
+    }
+
+}
---
+++ BJ/개인문제풀이/PGM/해결/짝수와홀수.java
@@ -0,0 +1,15 @@
+package BJ.개인문제풀이.PGM.해결;
+
+public class 짝수와홀수 {
+
+    public String solution(int num) {
+
+        if(num%2 == 0){
+            return "Even";
+        } else{
+            return "Odd";
+        }
+
+    }
+
+}
