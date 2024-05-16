cd61587168aeeaf96b34ce97f2122e2901805f0e
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-16 21:56:14 +0900

    "압축" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/압축_1.java
@@ -0,0 +1,45 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class 압축_1 {
+
+    public ArrayList<Integer> solution(String msg) {
+
+        int[] answer = {};
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        int idx =1;
+        HashMap<String, Integer> hs = new HashMap<>();
+
+        for(char i='A'; i<='Z'; i++){
+            hs.put(i+"", idx++);
+        }
+
+        int size = msg.length();
+
+        for(int i=0; i<size; i++){
+
+            int a = 1;
+
+            while(i+a<=size && hs.containsKey(msg.substring(i, i+a))){
+                a++;
+            }
+
+            if(i+a > size){
+                ans.add(hs.get(msg.substring(i)));
+                break;
+            }
+
+            ans.add(hs.get(msg.substring(i,i+a-1)));
+            hs.put(msg.substring(i,i+a), idx++);
+            if(a>1) i +=a-2;
+
+        }
+
+        return ans;
+
+    }
+
+}
