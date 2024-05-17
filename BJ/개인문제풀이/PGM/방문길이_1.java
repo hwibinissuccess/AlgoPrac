f1b0c28d661ed9d7582efca6af1c2e9720563f66
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-17 22:26:45 +0900

    "방문길이" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/방문길이_1.java
@@ -0,0 +1,45 @@
+package BJ.개인문제풀이.PGM;
+
+public class 방문길이_1 {
+
+    static int dr[] = {-1,0,1,0};
+    static int dc[] = {0,-1,0,1};
+
+    public int solution(String dirs) {
+
+        int ans = 0;
+        int map[][] = new int[11][11];
+        boolean visit[][][] = new boolean[11][11][4];
+        int r =5, c=5;
+
+        for(int i=0; i<dirs.length(); i++){
+
+            char cc = dirs.charAt(i);
+            int d = 0;
+
+            if(cc == 'U') d = 0;
+            if(cc == 'L') d = 1;
+            if(cc == 'D') d = 2;
+            if(cc == 'R') d = 3;
+
+            int nr = r+dr[d];
+            int nc = c+dc[d];
+
+            if(nr<0 || nc<0 || nr >=11 || nc >= 11) continue;
+
+            if(!visit[nr][nc][d]){
+                visit[nr][nc][d] = true;
+                d = (d%2==0) ? 2-d : 4-d;
+                visit[r][c][d] = true;
+                ans++;
+            }
+
+            r = nr;
+            c = nc;
+        }
+
+        return ans;
+
+    }
+
+}
