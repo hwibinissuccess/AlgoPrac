7caaf26c44cb343756b50d449a9202e44c6f1e54
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-20 23:34:33 +0900

    "카운트다운" 문제 해결

--- BJ/개인문제풀이/PGM/해결/카운트다운.java
+++ BJ/개인문제풀이/PGM/해결/카운트다운.java
@@ -1,25 +1,15 @@
-package BJ.개인문제풀이.PGM;
+package BJ.개인문제풀이.PGM.해결;
 
-public class 카운트다운_6 {
-    int[][] dp;
+public class 카운트다운 {
 
     public int[] solution(int target) {
 
-        int[] ans = new int[2];
-
-        dp = new int[target+1][2];
-
-        for(int i=0; i<=target; i++){
-            dp[i][0] = Integer.MAX_VALUE;
-            dp[i][1] = 0;
-        }
-
+        int[][] dp = new int[target+1][2];
+        for(int i=0; i<=target; i++) dp[i][0] = Integer.MAX_VALUE;
         dp[0][0] = 0;
 
         for(int i=1; i<=target; i++){
             for(int j=1; j<=20; j++){
-
-                int b_or_s = -1;
 
                 if(i-50>=0){
                     if(dp[i][0]>dp[i-50][0]+1){
@@ -31,7 +21,7 @@ public class 카운트다운_6 {
                 }
 
                 if(i-j>=0){
-                    if(dp[i][0] > dp[i-j][0]+1){
+                    if(dp[i][0]>dp[i-j][0]+1){
                         dp[i][0] = dp[i-j][0]+1;
                         dp[i][1] = dp[i-j][1]+1;
                     } else if(dp[i][0] == dp[i-j][0]+1){
@@ -39,22 +29,24 @@ public class 카운트다운_6 {
                     }
                 }
 
-                if(i-2*j>=0){
-                    if(dp[i][0] > dp[i-2*j][0]+1){
-                        dp[i][0] = dp[i-2*j][0]+1;
-                        dp[i][1] = dp[i-2*j][1];
+                if(i-(2*j)>=0){
+                    if(dp[i][0] > dp[i-(2*j)][0]+1){
+                        dp[i][0] = dp[i-(2*j)][0]+1;
+                        dp[i][1] = dp[i-(2*j)][1];
                     }
                 }
 
-                if(i-3*j>=0){
-                    if(dp[i][0] > dp[i-3*j][0]+1){
-                        dp[i][0] = dp[i-3*j][0]+1;
-                        dp[i][1] = dp[i-3*j][1];
+                if(i-(3*j)>=0){
+                    if(dp[i][0] > dp[i-(3*j)][0]+1){
+                        dp[i][0] = dp[i-(3*j)][0]+1;
+                        dp[i][1] = dp[i-(3*j)][1];
                     }
                 }
 
             }
         }
+
+        int[] ans = new int[2];
 
         ans[0] = dp[target][0];
         ans[1] = dp[target][1];
