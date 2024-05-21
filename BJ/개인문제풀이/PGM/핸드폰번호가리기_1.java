3c8a04b6c4eeb3fc4b9055407a8371e5474559a7
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-21 22:44:17 +0900

    "핸드폰번호가리기" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/핸드폰번호가리기_1.java
@@ -0,0 +1,17 @@
+package BJ.개인문제풀이.PGM;
+
+public class 핸드폰번호가리기_1 {
+
+    public String solution(String phone_number) {
+
+        String ans = "";
+        int len = phone_number.length();
+
+        for(int i=0; i<len; i++){
+
+            if(i<len-4) ans += "*";
+            else ans += phone_number.charAt(i);
+        }
+        return ans;
+    }
+}
