ced080c2ac6d6ad928e664d9c95f6cde07a4e7de
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-22 17:22:38 +0900

    "제일작은수제거하기" 문제 해결

---
+++ BJ/개인문제풀이/PGM/제일작은수제거하기_SSS.java
@@ -0,0 +1,26 @@
+package BJ.개인문제풀이.PGM;
+
+public class 제일작은수제거하기_SSS {
+
+    public int[] solution(int[] arr) {
+
+        if(arr.length == 1) return new int[]{-1};
+
+        int[] ans = new int[arr.length-1];
+        int minNum = Integer.MAX_VALUE;
+
+        for(int i=0; i<arr.length; i++){
+            minNum = Math.min(minNum, arr[i]);
+        }
+        int idx = 0;
+        for(int i=0; i<arr.length; i++){
+
+            if(arr[i] == minNum) continue;
+            ans[idx] = arr[i];
+            idx++;
+        }
+
+        return ans;
+    }
+
+}
