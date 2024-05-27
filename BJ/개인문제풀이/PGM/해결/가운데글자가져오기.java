51e2c1c65fc98513db1dd378dba47a552af2b3c6
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-27 17:55:31 +0900

    "가운데글자가져오기" 문제 해결

---
+++ BJ/개인문제풀이/PGM/해결/가운데글자가져오기.java
@@ -0,0 +1,20 @@
+package BJ.개인문제풀이.PGM.해결;
+
+public class 가운데글자가져오기 {
+
+    public String solution(String s) {
+
+        String ans = "";
+        int mid = s.length()/2;
+        int len = s.length();
+
+        if(len%2 == 0){
+            ans = s.substring(mid-1, mid+1);
+
+        }else{
+            ans = s.substring(mid,mid+1);
+        }
+
+        return ans;
+    }
+}
