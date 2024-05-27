98b24bdbed51f416a1c6e9303303d987f5665545
Choi Hwi bin <chlgnlqls3@nate.com>
2024-05-27 18:13:37 +0900

    "오픈채팅방" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/오픈채팅방_1.java
@@ -0,0 +1,44 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 오픈채팅방_1 {
+
+    public String[] solution(String[] record) {
+
+        Map<String, String> map = new HashMap<>();
+        int cnt = 0;
+
+        for(int i=0; i<record.length; i++){
+
+            String[] info = record[i].split(" ");
+
+            if(info[0].equals("Leave")){
+                continue;
+            }else if(info[0].equals("Enter")){
+                map.put(info[1], info[2]);
+            } else{
+                map.put(info[1], info[2]);
+                cnt++;
+            }
+        }
+
+        String[] ans = new String[record.length-cnt];
+        int idx = 0;
+
+        for(int i=0; i<record.length; i++){
+
+            String[] info = record[i].split(" ");
+            String nickname = map.get(info[1]);
+
+            if(info[0].equals("Enter")){
+                ans[idx++] = nickname+"님이 들어왔습니다.";
+            } else if(info[0].equals("Leave")){
+                ans[idx++] = nickname+"님이 나갔습니다.";
+            }
+        }
+
+        return ans;
+    }
+}
