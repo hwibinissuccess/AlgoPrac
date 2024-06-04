5507a21f8eed8ac5e4d7cc04dc23221bd13ad638
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-04 22:12:13 +0900

    "보행자 천국" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/보행자천국_1.java
@@ -0,0 +1,33 @@
+package BJ.개인문제풀이.PGM;
+
+public class 보행자천국_1 {
+
+    int MOD = 20170805;
+    public int solution(int m, int n, int[][] cityMap) {
+
+        int ans = 0;
+        int[][][] dp = new int[2][m+1][n+1];
+
+        dp[0][0][0] = 1;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+
+                if(cityMap[i][j] == 0){
+                    dp[0][i+1][j] += (dp[0][i][j]+dp[1][i][j])%MOD;
+                    dp[1][i][j+1] += (dp[0][i][j]+dp[1][i][j])%MOD;
+
+                } else if(cityMap[i][j] == 2){
+
+                    dp[0][i+1][j] += dp[0][i][j];
+                    dp[0][i+1][j] %= MOD;
+
+                    dp[1][i][j+1] += dp[1][i][j];
+                    dp[1][i][j+1] %= MOD;
+                }
+            }
+        }
+
+        return (dp[0][m-1][n-1] + dp[1][m-1][n-1])%MOD;
+    }
+}
