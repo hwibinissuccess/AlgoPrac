60c18c4d1d3632097b4a88df9d47702be361f7d1
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-04 22:01:43 +0900

    "삼각달팽이" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/삼각달팽이_1.java
@@ -0,0 +1,39 @@
+package BJ.개인문제풀이.PGM;
+
+public class 삼각달팽이_1 {
+
+    public int[] solution(int n) {
+
+        int[] ans = new int[(n*(n+1))/2];
+        int[][] map = new int[n][n];
+
+        int x = -1, y= 0;
+        int num =1;
+
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+
+                if(i%3 == 0) x++;
+                else if(i%3 == 1) y++;
+                else if(i%3 == 2){
+                    x--;
+                    y--;
+                }
+                map[x][y] = num++;
+            }
+        }
+
+        int k=0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(map[i][j] == 0){
+                    break;
+                }
+                ans[k++] = map[i][j];
+            }
+        }
+
+        return ans;
+    }
+}
