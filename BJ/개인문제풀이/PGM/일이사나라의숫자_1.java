43f7792e6961fe3336b6c6577ccc82553767e534
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-05 23:03:04 +0900

    "일이사나라의숫자" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/일이사나라의숫자_1.java
@@ -0,0 +1,21 @@
+package BJ.개인문제풀이.PGM;
+
+public class 일이사나라의숫자_1 {
+    public String solution(int n) {
+
+        String ans = "";
+        String[] nums = {"4","1","2"};
+
+        while(n > 0){
+
+            int r = n%3;
+            n /= 3;
+
+            if(r == 0) n--;
+
+            ans = nums[r]+ans;
+        }
+
+        return ans;
+    }
+}
