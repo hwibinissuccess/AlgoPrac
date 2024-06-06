fdf0bee96fc3226822831caa19fab9ba8e6c1853
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-06 19:41:05 +0900

    "마법의엘리베이터" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/마법의엘리베이터_1.java
@@ -0,0 +1,27 @@
+package BJ.개인문제풀이.PGM;
+
+public class 마법의엘리베이터_1 {
+
+    public int solution(int storey) {
+        int answer = 0;
+
+        while (storey != 0) {
+            int one = storey % 10; //  1의 자리
+            int ten = (storey / 10) % 10; // 10의 자리
+
+            if (one > 5) { //  올림
+                answer += (10 - one);
+                storey += 10;
+            } else if (one == 5) { // 10의 자리가 5이상이면 올림, 5미만이면 내림
+                answer += one;
+                storey += (ten < 5 ? 0 : 10);
+            } else { //  내림
+                answer += one;
+            }
+
+            storey /= 10;
+        }
+
+        return answer;
+    }
+}
