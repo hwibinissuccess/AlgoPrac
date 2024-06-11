156dba732ce9b2f308be760590dea2f4558fa727
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-11 17:49:51 +0900

    "시소짝꿍" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/시소짝꿍_1.java
@@ -0,0 +1,34 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class 시소짝꿍_1 {
+
+    public long solution(int[] weights) {
+
+        long ans = 0;
+        Map<Double, Integer> map = new HashMap<>();
+
+        Arrays.sort(weights);
+
+        for(int i : weights){
+
+            double a = i*1.0;
+            double b = (i*2.0)/3.0;
+            double c = (i*1.0)/2.0;
+            double d = (i*3.0)/4.0;
+
+            if(map.containsKey(a)) ans += map.get(a);
+            if(map.containsKey(b)) ans += map.get(b);
+            if(map.containsKey(c)) ans += map.get(c);
+            if(map.containsKey(d)) ans += map.get(d);
+
+            map.put((i*1.0), map.getOrDefault((i*1.0),0)+1);
+
+        }
+
+        return ans;
+    }
+}
