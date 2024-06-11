441248796fdb324f4e7156cb88dc970636c69318
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-11 17:36:58 +0900

    "크기가작은부분문자열" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/크기가작은부분문자열_1.java
@@ -0,0 +1,24 @@
+package BJ.개인문제풀이.PGM;
+
+public class 크기가작은부분문자열_1 {
+
+    public int solution(String t, String p) {
+
+        int cnt = 0;
+        int len = p.length();
+        long target = Long.parseLong(p);
+
+        for(int i=0; i<t.length()-len+1; i++){
+            StringBuilder num = new StringBuilder();
+            for(int j=i; j<i+len; j++){
+                num.append(t.charAt(j));
+            }
+
+            long nums = Long.parseLong(num.toString());
+
+            if(target >= nums) cnt++;
+        }
+
+        return cnt;
+    }
+}
