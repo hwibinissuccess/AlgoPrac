f730856e80d5e5bc5ff0f978ef9d2360819e9f5b
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-19 12:34:05 +0900

    "이상한문자만들기" 문제 해결

---
+++ BJ/개인문제풀이/PGM/이상한문자만들기_SSS.java
@@ -0,0 +1,29 @@
+package BJ.개인문제풀이.PGM;
+
+public class 이상한문자만들기_SSS {
+
+    public String solution(String s) {
+
+        String ans = "";
+        int idx = 0;
+
+        for(int i=0; i<s.length(); i++){
+
+            if(s.charAt(i) != ' '){
+
+                if(idx%2 == 0){
+                    ans += String.valueOf(s.charAt(i)).toUpperCase();
+                    idx++;
+                } else{
+                    ans += String.valueOf(s.charAt(i)).toLowerCase();
+                    idx++;
+                }
+            } else{
+                ans += " ";
+                idx = 0;
+            }
+        }
+
+        return ans;
+    }
+}
