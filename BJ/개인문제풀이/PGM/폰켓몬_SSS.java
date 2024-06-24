cc794b9c2e37b7dab483956138b0f0b83bcadda4
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-24 23:37:19 +0900

    "폰켓몬" 문제 해결

---
+++ BJ/개인문제풀이/PGM/폰켓몬_SSS.java
@@ -0,0 +1,26 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 폰켓몬_SSS {
+
+    public int solution(int[] nums) {
+
+        int ans = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+
+        int size =  map.size();
+        int len = nums.length/2;
+
+        if(size >= len) ans = len;
+        else ans = size;
+
+        return ans;
+    }
+}
