51f5b6feb225c099e9505a8db6cd23d87f7102f7
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-25 00:05:11 +0900

    "완주하지못한선수" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/완주하지못한선수_1.java
@@ -0,0 +1,34 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 완주하지못한선수_1 {
+
+    public String solution(String[] participant, String[] completion) {
+
+        String ans = "";
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String s : participant) {
+            if (!map.containsKey(s)) {
+                map.put(s, 1);
+            } else {
+                map.put(s, map.get(s) + 1);
+            }
+        }
+
+        for (String s : completion) {
+            map.put(s, map.get(s) - 1);
+        }
+
+        for (String s : map.keySet()) {
+            if (map.get(s) == 1) {
+                ans = s;
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
