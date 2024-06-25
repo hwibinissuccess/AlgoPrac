d47c1479f922d7ff609620e4d502dafa1c430e6d
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-25 16:56:39 +0900

    "가장큰수" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/가장큰수_1.java
@@ -0,0 +1,29 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.Arrays;
+
+public class 가장큰수_1 {
+
+    public String solution(int[] numbers) {
+
+        String[] arr = new String[numbers.length];
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(arr, (o1, o2) -> (o2+o1).compareTo(o1+o2));
+
+        if(arr[0].equals("0")){
+            return "0";
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=0; i<arr.length; i++){
+            ans.append(arr[i]);
+        }
+
+        return ans.toString();
+    }
+}
