03f3e9a2c3074a7a622455079a94c7846ae932b8
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-25 17:07:01 +0900

    "hindex" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/hindex_1.java
@@ -0,0 +1,24 @@
+package BJ.개인문제풀이.PGM;
+
+import java.util.Arrays;
+
+public class hindex_1 {
+
+    public int solution(int[] citations) {
+
+        int ans = 0;
+
+        Arrays.sort(citations);
+
+        for(int i=0; i<citations.length; i++){
+            int h = citations.length - i;
+
+            if(citations[i] >= h){
+                ans = h;
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
