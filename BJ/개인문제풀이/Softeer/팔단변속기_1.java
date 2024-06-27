05228167efb03f54a3a9ed1e703108b4ae90e70b
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-28 01:19:28 +0900

    "8단변속기" 문제 풀이

---
+++ BJ/개인문제풀이/Softeer/팔단변속기_1.java
@@ -0,0 +1,28 @@
+package BJ.개인문제풀이.Softeer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 팔단변속기_1 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] asc = {1,2,3,4,5,6,7,8};
+        int[] dec = {8,7,6,5,4,3,2,1};
+        String[] input = br.readLine().split(" ");
+        int[] num = new int[8];
+
+        for(int i=0; i<8; i++){
+            num[i] = Integer.parseInt(input[i]);
+        }
+
+        if(Arrays.equals(asc,num)) System.out.println("ascending");
+        else if(Arrays.equals(dec, num)) System.out.println("descending");
+        else System.out.println("mixed");
+        br.close();
+
+    }
+}
