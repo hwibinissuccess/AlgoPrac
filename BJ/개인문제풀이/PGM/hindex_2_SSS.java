2cad52860fcb02eb62ca70b621e2f165ac964a7e
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-27 23:04:17 +0900

    "hindex" 문제 해결

--- BJ/개인문제풀이/PGM/hindex_2_SSS.java
+++ BJ/개인문제풀이/PGM/hindex_2_SSS.java
@@ -2,7 +2,7 @@ package BJ.개인문제풀이.PGM;
 
 import java.util.Arrays;
 
-public class hindex_2 {
+public class hindex_2_SSS {
 
     public int solution(int[] citations) {
 
---
+++ BJ/개인문제풀이/PGM/해결/hindex.java
@@ -0,0 +1,23 @@
+package BJ.개인문제풀이.PGM.해결;
+
+import java.util.Arrays;
+
+public class hindex {
+
+    public int solution(int[] citations) {
+
+        int ans = citations.length;
+        Arrays.sort(citations);
+
+        for(int num : citations){
+
+            if(num >= ans){
+                break;
+            }
+
+            ans--;
+        }
+
+        return ans;
+    }
+}
