e2faca3f55bc0ecf0e0da111b3e87ce2998bf823
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-28 20:12:50 +0900

    미해결 문제 풀이

--- BJ/개인문제풀이/Softeer/금고털이_2.java
+++ BJ/개인문제풀이/Softeer/금고털이_2.java
@@ -5,7 +5,7 @@ import java.io.*;
 import java.util.*;
 
 
-public class 금고털이_1 {
+public class 금고털이_2 {
 
     public static void main(String[] args) throws IOException {
 
--- BJ/개인문제풀이/Softeer/팔단변속기_2.java
+++ BJ/개인문제풀이/Softeer/팔단변속기_2.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class 팔단변속기_1 {
+public class 팔단변속기_2 {
 
     public static void main(String[] args) throws IOException {
 
