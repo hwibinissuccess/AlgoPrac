3fd4c51021ee104ae8ae17c592b106a43e4926d8
Choi Hwi bin <chlgnlqls3@nate.com>
2024-06-28 23:55:59 +0900

    "지도자동구축" 문제 해결

---
+++ BJ/개인문제풀이/Softeer/지도자동구축_SSS.java
@@ -0,0 +1,27 @@
+package BJ.개인문제풀이.Softeer;
+import java.io.*;
+import java.util.*;
+
+public class 지도자동구축_SSS {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int line = 2;
+        int add = 1;
+
+        for(int i=0; i<N; i++){
+            if(i == 0){
+                line += add;
+                continue;
+            }
+            add *= 2;
+            line += add;
+        }
+
+        int ans = line*line;
+
+        System.out.println(ans);
+    }
+}
