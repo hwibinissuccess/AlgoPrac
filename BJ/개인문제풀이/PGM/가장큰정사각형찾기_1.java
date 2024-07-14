3fe430dc2d28b02161f95773041ba63fa1c2727b
Choi Hwi bin <chlgnlqls3@nate.com>
2024-07-15 00:48:53 +0900

    "가장큰정사각형찾기" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/가장큰정사각형찾기_1.java
@@ -0,0 +1,26 @@
+package BJ.개인문제풀이.PGM;
+
+public class 가장큰정사각형찾기_1 {
+
+    static int[][] map;
+
+    public int solution(int [][]board){
+
+        map = new int[board.length+1][board[0].length+1];
+        int maxLen = 0;
+
+        for(int i=1; i<=board.length; i++){
+            for(int j=1; j<=board[0].length; j++){
+                if(board[i-1][j-1] != 0){
+                    int min = Math.min(Math.min(map[i-1][j], map[i][j-1]),map[i-1][j-1]);
+
+                    map[i][j] = min+1;
+
+                    maxLen = Math.max(maxLen, min+1);
+                }
+            }
+        }
+
+        return maxLen*maxLen;
+    }
+}
