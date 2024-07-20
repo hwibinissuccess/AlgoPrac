268893761f2dc48d383c0d97c0a943e5ce741454
Choi Hwi bin <chlgnlqls3@nate.com>
2024-07-20 20:40:10 +0900

    "괄호변환" 문제 풀이

---
+++ BJ/개인문제풀이/PGM/괄호변환_1.java
@@ -0,0 +1,68 @@
+package BJ.개인문제풀이.PGM;
+
+public class 괄호변환_1 {
+
+    public String solution(String p) {
+
+        if(p.isEmpty()) return p;
+
+        int left = 0;
+        int right = 0;
+        int index = 0;
+
+        for(int i=0; i<p.length(); i++){
+
+            if(p.charAt(i) == '('){
+                left++;
+            } else{
+                right++;
+            }
+
+            if(left == right){
+                index = i;
+                break;
+            }
+        }
+
+        String u = p.substring(0,index+1);
+        String v = p.substring(index+1);
+
+        if(isCorrect(u)) return u + solution(v);
+
+        return "(" + solution(v) + ")" + reverse(u);
+    }
+
+    public static boolean isCorrect(String u){
+
+        int left = 0;
+        int right = 0;
+
+        for(int i=0; i<u.length(); i++){
+            if(u.charAt(i) == '('){
+                left++;
+            } else{
+                right++;
+            }
+
+            if(right > left) return false;
+        }
+
+        return true;
+    }
+
+    public static String reverse(String u){
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1; i<u.length()-1; i++){
+
+            if(u.charAt(i) == '('){
+                sb.append(')');
+            } else{
+                sb.append('(');
+            }
+        }
+
+        return sb.toString();
+    }
+}
