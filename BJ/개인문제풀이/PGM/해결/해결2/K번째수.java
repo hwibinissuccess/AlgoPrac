fe3add1411f141a52b5aa1815128a04131960fae
Choi Hwi bin <chlgnlqls3@nate.com>
2024-07-21 16:07:26 +0900

    해결문제 재풀이 및 해결

---
+++ BJ/개인문제풀이/PGM/해결/해결2/K번째수.java
@@ -0,0 +1,31 @@
+package BJ.개인문제풀이.PGM.해결.해결2;
+
+import java.util.Arrays;
+
+public class K번째수 {
+
+    public int[] solution(int[] array, int[][] commands) {
+
+        int[] ans = new int[commands.length];
+
+        for(int a=0; a<commands.length; a++){
+
+            int i = commands[a][0]-1;
+            int j = commands[a][1]-1;
+            int k = commands[a][2]-1;
+            int[] temp = new int[j-i+1];
+            int idx = 0;
+            for(int b=i; b<=j; b++){
+                temp[idx] = array[b];
+                idx++;
+            }
+
+            Arrays.sort(temp);
+
+            ans[a] = temp[k];
+
+        }
+
+        return ans;
+    }
+}
