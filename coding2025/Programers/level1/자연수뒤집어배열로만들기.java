278c3c04ab4d2507a82cd731673767fb2a1d3f0d
brian <chlgnlqls3@nate.com>
2025-03-20 22:59:59 +0900

    가장많이받은선물

---
+++ coding2025/Programers/level1/자연수뒤집어배열로만들기.java
@@ -0,0 +1,16 @@
+package coding2025.Programers.level1;
+
+public class 자연수뒤집어배열로만들기 {
+    public int[] solution(long n) {
+
+        String[] str = Long.toString(n).split("");
+
+        int[] ans = new int[str.length];
+
+        for(int i=0; i<str.length; i++){
+            ans[i] = Integer.parseInt(str[str.length-1-i]);
+        }
+
+        return ans;
+    }
+}
