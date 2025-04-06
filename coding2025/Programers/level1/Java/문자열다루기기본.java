be6b87f95ca9109c2b180c323c0cceddb133f3b3
brian <chlgnlqls3@nate.com>
2025-04-06 19:22:58 +0900

    문자열다루기기본_py,java

---
+++ coding2025/Programers/level1/Java/문자열다루기기본.java
@@ -0,0 +1,16 @@
+package coding2025.Programers.level1.Java;
+
+public class 문자열다루기기본 {
+
+
+    public boolean solution(String s) {
+
+        if(s.length() != 4 && s.length() != 6) return false;
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) < '0' || s.charAt(i)>'9') return false;
+        }
+
+        return true;
+    }
+}
