1b24edf3df47787e61576f8c8a9d9799a84d05ba
brian <chlgnlqls3@nate.com>
2025-04-20 23:10:50 +0900

    숫자문자열과영단어

---
+++ coding2025/Programers/level1/Java/숫자문자열과영단어.java
@@ -0,0 +1,17 @@
+package coding2025.Programers.level1.Java;
+
+public class 숫자문자열과영단어 {
+
+    public int solution(String s) {
+
+        String[] arr = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+        for(int i=0; i<arr.length; i++){
+            if(s.contains(arr[i])){
+                s = s.replace(arr[i], Integer.toString(i));
+            }
+        }
+
+        return Integer.valueOf(s);
+    }
+}
